8c04e803130282cac55dcea433104a42b76fe2e9
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-09-21 18:06:51 +0200

    Initialize Express App and connect to MongoDB with mongoose

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+// config env
+require('dotenv').config();
+
+
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(process.env.PORT || 4000, () => {
+      console.log(`Listening on port ${process.env.PORT}`);
+    });
+    console.log('Connected to DB');
+  });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+module.exports = app;
