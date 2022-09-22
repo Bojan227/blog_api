72bf5905a9063aa80715605e14893d2f116a8764
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-09-22 17:13:44 +0200

    Add controller for signing up users

---
+++ controllers/userController.js
@@ -0,0 +1,24 @@
+const User = require('../models/userModel');
+var jwt = require('jsonwebtoken');
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET_KEY, { expiresIn: '3d' });
+};
+
+const signupUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.signup(username, password);
+
+    res.status(200).json({ message: 'Successfully singed up' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+
+
+module.exports = {
+    signupUser
+}
